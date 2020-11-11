18450eee43faf0e4dd0e51b8fee5b6ae9d0bf68c
unknown <Raikomarrandi>
2020-11-11 14:27:35 +0200

    lisasin read_html faili natuke infot ja millest võiks ehk alustada

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #alusta hiljemalt 11. novembril
+isik = {"eesnimi":3}
+isik["eesnimi"] += 1
+print(isik["eesnimi"])
+    
+"""
+summa = 0
+for m in range(1,4):
+    for n in range(1,3):
+        print(summa)
+        summa+=n
+print(summa)"""
