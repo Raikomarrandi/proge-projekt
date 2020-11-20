5a650ccc654bdd4153c01ef8f89442df729917b6
raikomarrandi <Raikomarrandi>
2020-11-20 16:16:57 +0200

    htmlist päriselt lugemine

--- funktsioonid.py
+++ funktsioonid.py
@@ -4,14 +4,20 @@ from bs4 import BeautifulSoup
 #siin failis abifunktsioonid
 
 #url for testing = 'http://www.okvoru.ee/site/wp-content/uploads/2020/09/EMV_tavarada050920_etapid_a1.htm'
+#file:///C:/Users/Raiko/Desktop/T%C3%9C%20proge/proge-projekt/Tulemused.html
+
 def read_html(url):
-        http = urllib3.PoolManager()
-
-
-        response = http.request('GET', url)
-        soup = BeautifulSoup(response.data,'html.parser')
-
-
-        print(soup.title)
-        print(soup.a)
-        print(soup.b)
+    html_doc = open("Tulemused.html",encoding="utf-8")
+    soup = BeautifulSoup(html_doc,'html.parser')
+    soup = soup.find("pre")
+    [x.extract() for x in soup.select('span')]
+    aba = soup.getText()
+    aba = aba.split("\n")
+    for i in aba:
+        if i == "" or i == " ":
+            aba.remove(i)
+    võistlejad={}
+    for i in range(len(aba)):
+        vend = aba[i].split()
+        võistlejad[str(i+1)] = vend[1:]
+    return võistlejad["1"]
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from funktsioonid import read_html
 def main():
     #klass = input("Sisesta võistlusklass: ")
-    url ='http://www.joka.ee/wp/wp-content/uploads/2020/10/tulemused_etapp_esialgne'
-    read_html(url)
+    #url ='http://www.joka.ee/wp/wp-content/uploads/2020/10/tulemused_etapp_esialgne'
+    url = "Tulemused.html"
+    print(read_html(url))
     
 
 main()
